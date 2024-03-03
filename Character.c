38b4bfd36778ee48d0219fb99fe758f7198fee0f
el_sime <el_sime@el-sime.net>
2024-03-03 23:19:54 +0100

    WIP(Character) basic attributes and factory

---
+++ Character.c
@@ -0,0 +1,20 @@
+#include "Character.h"
+
+#include <string.h>
+
+Character CharacterFactory(int level, CharacterType type)
+{
+    Character newChar;
+    strcpy(newChar.Name, "Citizen");
+    newChar.Level = type == PLAYER ? 1 : level;
+    newChar.XP = 0;
+    newChar.CharacterPoints = 0;
+    newChar.SkillPoints = 0;
+    newChar.Type = type;
+    int hp = 90 + level * 10;
+    newChar.HitPoints.baseValue = newChar.HitPoints.currentValue = newChar.HitPoints.maxValue = hp;
+    int ap = 5 + level;
+    newChar.ActionPoints.baseValue = newChar.ActionPoints.currentValue = newChar.ActionPoints.maxValue = ap;
+
+    return newChar;
+}
--- Character.h
+++ Character.h
@@ -13,7 +13,7 @@ typedef struct Attributes {
 } Attribute;
 
 typedef struct Character {
-    char *Name[100];
+    char Name[100];
     int Level;
     float XP;
     int CharacterPoints;
@@ -23,24 +23,5 @@ typedef struct Character {
     Attribute ActionPoints;
 } Character;
 
-Character CharacterFactory(int level, CharacterType type)
-{
-    Character newChar;
-    newChar.Name = "Citizen";
-    newChar.Level = type == PLAYER ? 1 : level;
-    newChar.XP = 0;
-    newChar.CharacterPoints = 0;
-    newChar.SkillPoints = 0;
-    newChar.Type = type;
-    int hp = 90 + level * 10;
-    newChar.HitPoints.baseValue = hp;
-    newChar.HitPoints.currentValue = hp;
-    newChar.HitPoints.maxValue = hp;
-    int ap = 5 + level;
-    newChar.ActionPoints.baseValue = ap;
-    newChar.ActionPoints.currentValue = ap;
-    newChar.ActionPoints.maxValue = ap;
-
-    return newChar;
-}
+Character CharacterFactory(int level, CharacterType type);
 #endif // CHARACTER_H
